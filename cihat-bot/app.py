04a4e09bc7a0cc5b9f3414d2da46de63b36042d4
gregrebin <g.s.rebinskiy@gmail.com>
2021-03-10 19:56:09 +0100

    Refactoring and documentation

--- cihat-bot/app.py
+++ cihat-bot/app.py
@@ -1,4 +1,4 @@
-from const import Constants
+from const import *
 from ui import Ui
 from trader import Trader
 from events import Listener
@@ -8,14 +8,14 @@ class Application(Listener):
 
     def __init__(self, ui_name: str, trader_name: str) -> None:
         """ Initialize an ui and a trader based on given names """
-        self.ui: Ui = Constants.UIS[ui_name](self)
-        self.trader: Trader = Constants.TRADERS[trader_name](self)
+        self.ui: Ui = UIS[ui_name](self)
+        self.trader: Trader = TRADERS[trader_name](self)
 
     def event(self, event: str) -> None:
         """ Receives an event, call the appropriate function,
-        operating on the trader in case of an ui event and on ui in case of the trader event """
-        if event in Constants.UI_EVENTS:
-            Constants.UI_EVENTS[event](self.trader)
-        elif event in Constants.TRADER_EVENTS:
-            Constants.TRADER_EVENTS[event](self.ui)
+        operating on the trader in case of an ui event and on th ui in case of a trader event """
+        if event in UI_EVENTS:
+            UI_EVENTS[event](self.trader)
+        elif event in TRADER_EVENTS:
+            TRADER_EVENTS[event](self.ui)
 
--- cihat-bot/const.py
+++ cihat-bot/const.py
@@ -3,20 +3,18 @@ from trader import Trader
 from typing import Dict, Type, Callable
 
 
-class Constants:
-    """ Concrete ui implementation classes """
-    UIS: Dict[str, Type[Ui]] = {
+""" Concrete ui implementation classes """
+UIS: Dict[str, Type[Ui]] = {
+}
 
-    }
-    """ Concrete trader implementation classes """
-    TRADERS: Dict[str, Type[Trader]] = {
+""" Concrete trader implementation classes """
+TRADERS: Dict[str, Type[Trader]] = {
+}
 
-    }
-    """ Events that can be emitted by ui, and the relative function on the trader """
-    UI_EVENTS: Dict[str, Callable[[Trader], None]] = {
+""" Events that can be emitted by ui, and the relative function on the trader """
+UI_EVENTS: Dict[str, Callable[[Trader], None]] = {
+}
 
-    }
-    """ Events that can be emitted by trader, and the relative function on the ui """
-    TRADER_EVENTS: Dict[str, Callable[[Ui], None]] = {
-
-    }
+""" Events that can be emitted by trader, and the relative function on the ui """
+TRADER_EVENTS: Dict[str, Callable[[Ui], None]] = {
+}
