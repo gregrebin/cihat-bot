aef96515876dd8e4c8b88c4580daf5492a422666
gregrebin <g.s.rebinskiy@gmail.com>
2021-03-17 11:56:56 +0100

    Implemented simple "ok bot"

--- cihatbot/uis/telegram.py
+++ cihatbot/uis/telegram.py
@@ -1,7 +1,8 @@
 from cihatbot.module import Module
 from configparser import SectionProxy
 from queue import Queue
-from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from threading import Event as ThreadEvent
 
 
@@ -10,6 +11,17 @@ class Telegram(Module):
     def __init__(self, config: SectionProxy, queue: Queue, exit_event: ThreadEvent):
         super().__init__(config, queue, exit_event)
 
-        self.bot = Bot(config["token"])
-        print(self.bot.getMe())
+        self.updater = Updater(config["token"])
+        self.dispatcher = self.updater.dispatcher
+        self.message_handler = MessageHandler(Filters.text, self._message_handler)
 
+        self.dispatcher.add_handler(self.message_handler)
+
+    def pre_run(self) -> None:
+        self.updater.start_polling()
+
+    def post_run(self):
+        self.updater.stop()
+
+    def _message_handler(self, update: Update, context: CallbackContext):
+        update.message.reply_text("ok")
