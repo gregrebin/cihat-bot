d1a11e4184fc7af8b65d46c5613dcb7491ddbac5
gregrebin <g.s.rebinskiy@gmail.com>
2021-04-12 18:58:42 +0200

    Change something in tests

--- cihatbot/test/async_module_test.py
+++ cihatbot/test/async_module_test.py
@@ -1,6 +1,36 @@
+from __future__ import annotations
 from cihatbot.module import Module
+from typing import Callable
 import asyncio
 import signal
+
+
+def inject_app(func: Callable[[Module, App], None]) -> Callable[[Module, str], None]:
+    def wrapped(self, name):
+        injected = Injector.get_app(name)
+        return func(self, injected)
+    return wrapped
+
+
+def inject_user(func: Callable[[Module, User], None]) -> Callable[[Module, str], None]:
+    def wrapped(self, name):
+        injected = Injector.get_user(name)
+        return func(self, injected)
+    return wrapped
+
+
+def inject_trader(func: Callable[[Module, Trader], None]) -> Callable[[Module, str], None]:
+    def wrapped(self, name):
+        injected = Injector.get_trader(name)
+        return func(self, injected)
+    return wrapped
+
+
+def inject_ui(func: Callable[[Module, Ui], None]) -> Callable[[Module, str], None]:
+    def wrapped(self, name):
+        injected = Injector.get_ui(name)
+        return func(self, injected)
+    return wrapped
 
 
 class Ui(Module):
@@ -48,16 +78,53 @@ class App(Module):
         self.add_submodule(self.user1)
         self.add_submodule(self.user2)
 
+    @inject_user
+    def add_user(self, user: User):
+        self.add_submodule(user)
+
+
+class Injector:
+
+    apps = {
+        "app": App
+    }
+    users = {
+        "user": User
+    }
+    traders = {
+        "trader": Trader
+    }
+    uis = {
+        "ui": Ui
+    }
+
+    @staticmethod
+    def get_app(name):
+        return Injector.apps[name]().init()
+
+    @staticmethod
+    def get_user(name):
+        return Injector.users[name]().init()
+
+    @staticmethod
+    def get_trader(name):
+        return Injector.traders[name]().init()
+
+    @staticmethod
+    def get_ui(name):
+        return Injector.uis[name]().init()
+
 
 async def main():
 
     print("test")
-    app = App().init()
+    app = Injector.get_app("app")
 
     loop = asyncio.get_event_loop()
     loop.add_signal_handler(signal.SIGINT, lambda: asyncio.create_task(app.stop()))
     loop.add_signal_handler(signal.SIGTERM, lambda: asyncio.create_task(app.stop()))
 
+    app.add_user("user")
     await app.run()
 
 
