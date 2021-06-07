2f74dc2746f1b79f1207ba6124975bfd59f9df3c
gregrebin <g.s.rebinskiy@gmail.com>
2021-06-07 23:06:09 +0200

    Defined order parser formal grammar and added a dummy sly lexer.

--- cihatbot/application/order.py
+++ cihatbot/application/order.py
@@ -5,7 +5,7 @@ from uuid import uuid4, UUID
 from enum import Enum, auto
 from dataclasses import dataclass, field, replace
 from abc import ABC, abstractmethod
-
+from sly import Lexer
 
 def new_uid():
     return uuid4().hex
@@ -131,351 +131,38 @@ class Multiple(Order):
             return replace(self, orders=orders)
 
 
-# @dataclass(frozen=True)
-# class Parallel(Multiple):
-#
-#     def add(self, order: Order) -> Order:
-#         pass
-#
-#     def get(self, status: Status = None, uid: str = None, pending: bool = False) -> List[Order]:
-#         pass
-#
-#     def update(self, status: Status) -> Order:
-#         pass
-#
-#     def cancel(self, uid: str) -> Order:
-#         pass
-#
-#
-# @dataclass(frozen=True)
-# class Sequent(Multiple):
-#
-#     def add(self, order: Order) -> Order:
-#         pass
-#
-#     def get(self, status: Status = None, uid: str = None, pending: bool = False) -> List[Order]:
-#         pass
-#
-#     def update(self, status: Status) -> Order:
-#         pass
-#
-#     def cancel(self, uid: str) -> Order:
-#         pass
-#
-#
-# @dataclass(frozen=True)
-# class Exclusive(Multiple):
-#
-#     def add(self, order: Order) -> Order:
-#         pass
-#
-#     def get(self, status: Status = None, uid: str = None, pending: bool = False) -> List[Order]:
-#         pass
-#
-#     def update(self, status: Status) -> Order:
-#         pass
-#
-#     def cancel(self, uid: str) -> Order:
-#         pass
+# Parser formal grammar:
+# ---------------
+# order ::= <empty> | <single> | <multiple>
+#   empty ::= ""
+#   single ::= <command> <quote> <symbol> in <exchange> at <price> | <command> <symbol> in <exchange> at <price> for <base>  (ex. buy 5 BTCUSDT in Binance at 20000 / buy BTCUSDT in Binance at 20000 for 1000)
+#       command ::= buy | sell
+#       quote ::= <decimal>
+#       symbol ::= <string>
+#       exchange ::= <string>
+#       price ::= <decimal>
+#       base ::= <string>
+#   multiple ::= [<mode> <orders>]
+#       mode ::= parallel | sequent
+#       orders ::= <order> | <order>, <orders>
+
+# https://sly.readthedocs.io/en/latest/sly.html
+# https://yahel-oppenheimer.medium.com/create-your-own-scripting-language-in-python-with-sly-7b864e762e07
+# https://www.rexegg.com/regex-quickstart.html
+
+# noinspection PyUnresolvedReferences,PyUnboundLocalVariable
+class OrderLexer(Lexer):
+
+    tokens = {COMMAND, MODE, IN, AT, FOR, DECIMAL, STRING}
+    literals = {","}
+
+    ignore = r" \t"
+
+    IN = r"in"
+    AT = r"at"
+    FOR = r"for"
+    COMMAND = r"(buy|sell)"
+    MODE = r"(parallel|sequent)"
+    DECIMAL = "\d+\.?\d*"
+    STRING = "\w+"
 
-
-def locked(no_lock: Callable):
-
-    def with_lock(self: ExecutionOrder, *args, **kwargs):
-        if self.lock.acquire():
-            return_value = no_lock(self, *args, **kwargs)
-            self.lock.release()
-            return return_value
-
-    return with_lock
-
-
-class ExecutionOrder:
-
-    def __init__(self, order_type: str):
-        self.order_type: str = order_type
-        self.status: OrderStatus = OrderStatus.PENDING
-        self.order_id: str = uuid4().hex
-        self.external_id: int = 0
-        self.lock: Lock = Lock()
-
-    def __str__(self):
-        return f"""{self.order_type} order"""
-
-    def add(self, order: ExecutionOrder, mode: str):
-        pass
-
-    def cancel(self, order_id: str):
-        pass
-
-    def submitted(self, order_id: str):
-        pass
-
-    def filled(self, order_id: str):
-        pass
-
-    def rejected(self, order_id: str):
-        pass
-
-    @locked
-    def add_parallel(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        return execution_order
-
-    @locked
-    def add_sequential(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        return execution_order
-
-    @locked
-    def remove(self, order_id: str = None, external_id: int = None) -> ExecutionOrder:
-        return EmptyExecutionOrder()
-
-    @locked
-    def call(self, func: Callable[[SingleExecutionOrder], None], order_id: str = None, external_id: int = None) -> bool:
-        pass
-
-    @locked
-    def submit(self, submit_func: Callable[[SingleExecutionOrder], OrderStatus]) -> None:
-        pass
-
-    @locked
-    def cancel(self, cancel_func: Callable[[SingleExecutionOrder], None], order_id: str = None, external_id: int = None) -> ExecutionOrder:
-        return self.remove()
-
-    def _is_order(self, order_id: str = None,  external_id: int = None):
-        return self.order_id == order_id or self.external_id == external_id or (not order_id and not external_id)
-
-
-class EmptyExecutionOrder(ExecutionOrder):
-
-    def __init__(self):
-        super().__init__("empty")
-
-
-class SingleExecutionOrder(ExecutionOrder):
-
-    def __init__(self, from_time: float, execution_params: ExecutionParams, execution_conditions: ExecutionConditions):
-        super().__init__("single")
-
-        self.from_time: float = from_time
-        self.params: ExecutionParams = execution_params
-        self.conditions: ExecutionConditions = execution_conditions
-
-    def __str__(self):
-        return f"""{self.params.command} {self.params.symbol} {self.params.price} {self.params.quantity}"""
-
-    @locked
-    def add_parallel(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        return ParallelExecutionOrder([self, execution_order])
-
-    @locked
-    def add_sequential(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        return SequentExecutionOrder([self, execution_order])
-
-    @locked
-    def remove(self, order_id: str = None,  external_id: int = None) -> ExecutionOrder:
-
-        if self._is_order(order_id=order_id, external_id=external_id):
-            return EmptyExecutionOrder()
-        else:
-            return self
-
-    @locked
-    def call(self, func: Callable[[SingleExecutionOrder], None], order_id: str = None, external_id: int = None) -> bool:
-
-        if self._is_order(order_id=order_id, external_id=external_id):
-            func(self)
-            return True
-        else:
-            return False
-
-    @locked
-    def submit(self, submit_func: Callable[[SingleExecutionOrder], OrderStatus]) -> None:
-
-        if self.status == OrderStatus.PENDING:
-            self.status = submit_func(self)
-
-    @locked
-    def cancel(self, cancel_func: Callable[[SingleExecutionOrder], None], order_id: str = None, external_id: int = None) -> ExecutionOrder:
-
-        if not self._is_order(order_id=order_id, external_id=external_id):
-            return self
-        elif self.status == OrderStatus.SUBMITTED:
-            cancel_func(self)
-            return self
-        else:
-            return self.remove()
-
-
-class MultipleExecutionOrder(ExecutionOrder):
-
-    def __init__(self, order_type, orders: List[ExecutionOrder]):
-        super().__init__(order_type)
-
-        if len(orders) == 0:
-            raise EmptyOrderList
-
-        self.orders = orders
-
-    def __str__(self):
-        orders = [str(order) for order in self.orders]
-        return f"""[{self.order_type} {', '.join(orders)}]"""
-
-    @locked
-    def remove(self, order_id: str = None,  external_id: int = None) -> ExecutionOrder:
-
-        if self._is_order(order_id=order_id, external_id=external_id):
-            return EmptyExecutionOrder()
-
-        new_orders = []
-        empty = True
-        for order in self.orders:
-            new_order = order.remove(order_id, external_id)
-            if not isinstance(new_order, EmptyExecutionOrder):
-                new_orders.append(new_order)
-                empty = False
-
-        if empty:
-            return EmptyExecutionOrder()
-        else:
-            self.orders = new_orders
-            return self
-
-    @locked
-    def call(self, func: Callable[[SingleExecutionOrder], None], order_id: str = None,  external_id: int = None) -> bool:
-        applied = False
-        for order in self.orders:
-            applied = order.call(func, order_id=order_id, external_id=external_id) or applied
-        return applied
-
-    @locked
-    def cancel(self, cancel_func: Callable[[SingleExecutionOrder], None], order_id: str = None, external_id: int = None) -> ExecutionOrder:
-
-        if self._is_order(order_id=order_id, external_id=external_id):
-            self.cancel(cancel_func)
-            return self.remove()
-
-        new_orders = []
-        empty = True
-        for order in self.orders:
-            new_order = order.cancel(cancel_func, order_id=order_id, external_id=external_id)
-            if not isinstance(new_order, EmptyExecutionOrder):
-                new_orders.append(new_order)
-                empty = False
-
-        if empty:
-            return EmptyExecutionOrder()
-        else:
-            self.orders = new_orders
-            return self
-
-    def _check_submitted(self):
-        for order in self.orders:
-            if not order.status == OrderStatus.SUBMITTED:
-                return
-        self.status = OrderStatus.SUBMITTED
-
-
-class ParallelExecutionOrder(MultipleExecutionOrder):
-
-    def __init__(self, orders: List[ExecutionOrder]):
-        super().__init__("parallel", orders)
-
-    @locked
-    def add_parallel(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        if isinstance(execution_order, ParallelExecutionOrder):
-            self.orders.extend(execution_order.orders)
-        else:
-            self.orders.append(execution_order)
-        self.status = OrderStatus.PENDING
-        return self
-
-    @locked
-    def add_sequential(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        return SequentExecutionOrder([self, execution_order])
-
-    @locked
-    def submit(self, submit_func: Callable[[SingleExecutionOrder], OrderStatus]):
-        if not self.status == OrderStatus.SUBMITTED:
-            for order in self.orders:
-                order.submit(submit_func)
-        self._check_submitted()
-
-
-class SequentExecutionOrder(MultipleExecutionOrder):
-
-    def __init__(self, orders: List[ExecutionOrder]):
-        super().__init__("sequent", orders)
-
-    @locked
-    def add_parallel(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        return ParallelExecutionOrder([self, execution_order])
-
-    @locked
-    def add_sequential(self, execution_order: ExecutionOrder) -> ExecutionOrder:
-        if isinstance(execution_order, SequentExecutionOrder):
-            self.orders.extend(execution_order.orders)
-        else:
-            self.orders.append(execution_order)
-        self.status = OrderStatus.PENDING
-        return self
-
-    @locked
-    def submit(self, submit_func: Callable[[SingleExecutionOrder], OrderStatus]):
-        if not self.status == OrderStatus.SUBMITTED:
-            self.orders[0].submit(submit_func)
-        self._check_submitted()
-
-
-class OrderStatus(Enum):
-
-    PENDING = auto()
-    SUBMITTED = auto()
-    REJECTED = auto()
-
-
-class ExecutionParams:
-
-    CMD_BUY = "buy"
-    CMD_SELL = "sell"
-
-    def __init__(self, command: str, symbol: str, price: float, quantity: float) -> None:
-
-        if not (command == ExecutionParams.CMD_BUY or command == ExecutionParams.CMD_SELL):
-            command = ExecutionParams.CMD_BUY
-
-        self.command: str = command
-        self.symbol: str = symbol
-        self.price: float = price
-        self.quantity: float = quantity
-
-
-class ExecutionConditions:
-
-    def __init__(self, min_price: float = 0, max_price: float = 0, reference: ExecutionParams = None):
-        self.min_p = min_price
-        self.max_p = max_price
-        self.reference = reference
-        print(f"""initialized conditions: min {self.min_p}, max {self.max_p}, ref {self.reference}""")
-
-    @staticmethod
-    def _percent(f: float):
-        return f / 100
-
-    def max_price(self) -> float:
-        if self.reference:
-            return self._percent(self.max_p) * self.reference.price
-        else:
-            return self.max_p
-
-    def min_price(self) -> float:
-        print(f"""getting min price: min {self.min_p}, max {self.max_p}, ref {self.reference}""")
-        if self.reference:
-            print(f"""with ref: {self._percent(self.min_p) * self.reference.price}""")
-            return self._percent(self.min_p) * self.reference.price
-        else:
-            print(f"""no ref: {self.min_p}""")
-            return self.min_p
-
-
-class EmptyOrderList(Exception):
-    pass
--- tests/application/test_order.py
+++ tests/application/test_order.py
@@ -1,4 +1,4 @@
-from cihatbot.application.order import Empty, Status
+from cihatbot.application.order import Empty, Status, OrderLexer
 import unittest
 
 
@@ -11,3 +11,12 @@ class TestOrder(unittest.TestCase):
         self.order = Empty()
         self.assertEqual(self.order.status, Status.NEW)
         print(self.order.uid)
+
+    def test_lexer(self):
+        data = "buy 0.5 BTCUSDT, 1 ETH in Binance at 55000"
+        lexer = OrderLexer()
+        for token in lexer.tokenize(data):
+            print(token.type)
+            print(token.value)
+
+
