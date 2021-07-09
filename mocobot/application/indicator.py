438483aa1edf72a3b495942f1d67a97490635856
gregrebin <g.s.rebinskiy@gmail.com>
2021-07-09 11:35:30 +0200

    Implement indicator wrapper (untested).

--- mocobot/application/indicator.py
+++ mocobot/application/indicator.py
@@ -1,71 +1,98 @@
+from pandas import DataFrame, Series
 from dataclasses import dataclass
-from dataclasses import dataclass
-from inspect import getmembers, isclass
-from typing import Dict
-import ta.momentum, ta.others, ta.trend, ta.volatility, ta.volume
-# import pandas_ta as ta
+from typing import Dict, Callable, Tuple
+from inspect import signature
+import pandas_ta as ta
+import re
 
 
-INDICATORS = {
-    "momentum": tuple(indicator[0] for indicator in getmembers(ta.momentum, isclass) if indicator[1].__module__ == "ta.momentum"),
-    "trend": tuple(indicator[0] for indicator in getmembers(ta.trend, isclass) if indicator[1].__module__ == "ta.trend"),
-    "volatility": tuple(indicator[0] for indicator in getmembers(ta.volatility, isclass) if indicator[1].__module__ == "ta.volatility"),
-    "volume": tuple(indicator[0] for indicator in getmembers(ta.volume, isclass) if indicator[1].__module__ == "ta.volume"),
-    "others": tuple(indicator[0] for indicator in getmembers(ta.others, isclass) if indicator[1].__module__ == "ta.others"),
-
+INDICATORS = DataFrame().ta.indicators(as_list=True)
+LINES = {
+    "brba": ("ar", "br"),
+    "dm": ("+dm", "-dm"),
+    "eri": ("bull", "bear"),
+    "kst": ("kst", "signal"),
+    "macd": ("macd", "histogram", "signal"),
+    "ppo": ("ppo", "histogram", "signal"),
+    "pvo": ("pvo", "histogram", "signal"),
+    "qqe": ("qqe", "basis", "long", "short"),
+    "smi": ("smi", "signal", "oscillator"),
+    "squeeze": ("sqz", "on", "off", "no"),
+    "squeeze_pro": ("sqz", "wide", "normal", "narrow", "off", "no"),
+    "stc": ("stc", "macd", "stoch"),
+    "stoch": ("k", "d"),
+    "stochrsi": ("k", "d"),
+    "td_seq": ("up", "down"),
+    "hilo": ("line", "long", "short"),
+    "supertrend": ("trend", "direction", "long", "short"),
+    "drawdown": ("drawdown", "percent", "log"),
+    "tos_stdevall": ("central", "lower", "upper"),  # TODO: check columns
+    "adx": ("adx", "dmp", "dmn"),
+    "amat": ("long", "short"),
+    "aroon": ("up", "down", "oscillator"),
+    "cksp": ("long", "short"),
+    "psar": ("long", "short", "af", "reversal"),
+    "tsignals": ("trends", "trades", "entries", "exits"),
+    "ttm_trend": "ttm_trend",
+    "vortex": ("vip", "vim"),
+    "xsignals": ("trends", "trades", "entries", "exits"),
+    "aberration": ("zg", "sg", "xg", "atr"),
+    "accbands": ("lower", "mid", "upper"),
+    "bbands": ("lower", "mid", "upper", "bandwidth", "percent"),
+    "donchian": ("lower", "mid", "upper"),
+    "hwc": ("mid", "upper", "lower"),
+    "kc": ("lower", "upper", "basis"),
+    "rvi": ("lower", "upper", "basis"),
+    "thermo": ("thermo", "ma", "long", "short"),
+    "aobv": ("obv", "min", "max", "fast", "slow", "long", "short"),
+    "kvo": ("kvo", "signal"),
 }
 
 
 @dataclass(frozen=True)
 class Indicator:
 
-    type: str
     name: str
     settings: Dict[str, int]
     min: int
     max: int
+    line: str = ""
 
     def __post_init__(self):
-        if self.type not in INDICATORS:
-            raise IndicatorError(f"Invalid indicator type - {self.type}")
-        if self.name not in INDICATORS[self.type]:
+        if self.name not in INDICATORS:
             raise IndicatorError(f"Invalid indicator name - {self.name}")
         if not self.min <= self.max:
             raise IndicatorError(f"Min should be less than or equal to max - min={self.min} max={self.max} in {self.name}")
-        # for setting in self.settings:
-        #     if setting not in self._settings:
-        #         raise IndicatorError(f"Invalid indicator setting - {setting} in {self.name}")
+        for setting in self.settings:
+            if setting not in self._settings:
+                raise IndicatorError(f"Invalid indicator setting - {setting} in {self.name}")
 
     @property
-    def _indicator(self):
-        return ...
+    def _function(self) -> Callable:
+        return object.__getattribute__(ta, self.name)
 
-    # @property
-    # def _function(self) -> Callable:
-    #     return object.__getattribute__(ta, self.name)
-    #
-    # @property
-    # def _data(self) -> Tuple[str]:
-    #     return tuple(param.name for param in signature(self._function).parameters.values()
-    #                  if param.kind == param.POSITIONAL_OR_KEYWORD and param.default == param.empty)
-    #
-    # @property
-    # def _settings(self) -> Tuple[str]:
-    #     return tuple(param.name for param in signature(self._function).parameters.values()
-    #                  if param.kind == param.POSITIONAL_OR_KEYWORD and not param.default == param.empty)
-    #
-    # def __call__(self, **data) -> bool:
-    #     for data_entry in self._data:
-    #         if data_entry not in data:
-    #             raise IndicatorError(f"Missing data entry - {data_entry} in {self.name}")
-    #     result = self._function(**data, **self.settings)
-    #     print(result)
-    #     if isinstance(result, Series):
-    #         return self.min < result.iloc[-1] < self.max
-    #     elif isinstance(result, DataFrame) and self.line in result.columns:
-    #         return self.min < result.loc[:, self.line].iloc[-1] < self.max
-    #     else:
-    #         return False
+    @property
+    def _data(self) -> Tuple[str]:
+        return tuple(param.name for param in signature(self._function).parameters.values()
+                     if param.kind == param.POSITIONAL_OR_KEYWORD and param.default == param.empty)
+
+    @property
+    def _settings(self) -> Tuple[str]:
+        return tuple(param.name for param in signature(self._function).parameters.values()
+                     if param.kind == param.POSITIONAL_OR_KEYWORD and not param.default == param.empty)
+
+    def __call__(self, **data) -> bool:
+        for data_entry in self._data:
+            if data_entry not in data:
+                raise IndicatorError(f"Missing data entry - {data_entry} in {self.name}")
+        result = self._function(**data, **self.settings)
+        print(result)
+        if isinstance(result, Series):
+            return self.min < result.iloc[-1] < self.max
+        elif isinstance(result, DataFrame) and self.name in LINES and self.line in LINES[self.name]:
+            return self.min < result.iloc[-1, LINES[self.name].index(self.line)] < self.max
+        else:
+            return False
 
 
 class IndicatorError(ValueError):
